61b1f625ff851d9a51c30a17af9b59fc1088823d
Kim Svahn <kim.svahn@msb.se>
2025-10-30 15:54:00 +0100

    .

--- sops-setup.sh
+++ sops-setup.sh
@@ -1,10 +1,33 @@
-#rm /tmp/age.*
-#age-keygen -o /tmp/age.key &> /tmp/age.pub
+#!/bin/bash
+
+rm /tmp/age.*
+age-keygen -o /tmp/age.key &> /tmp/age.pub
 
 kubectl delete secret -n flux-system sops-age
-
 kubectl create secret generic sops-age \
   --namespace=flux-system \
   --from-file=age.agekey=/tmp/age.key \
   --type=Opaque
 
+
+cat > .sops.yaml <<EOF
+creation_rules:
+  - path_regex: .*\\.yaml$
+    encrypted_regex: '^(data|stringData)$'
+    age:
+      - $(cat /tmp/age.pub)
+EOF
+
+PW=$(openssl rand -base64 32)
+cat > secrets/secret.yaml <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: my-secret
+  namespace: dev1
+type: Opaque
+stringData:
+  DB_PASSWORD: ${PW}
+EOF
+
+sops --encrypt -i secrets/secret.yaml
